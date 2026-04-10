3818553026b8d0de0ba69027d6593f53fc04b0a9
mithlesh kumar <158745208+Mithleshyadavgithub@users.noreply.github.com>
2026-04-11 00:00:58 +0530

    Fix python import path for Vercel WSGI

--- gyan_uday/wsgi.py
+++ gyan_uday/wsgi.py
@@ -1,4 +1,11 @@
 import os
+import sys
+
+# Ensure the root project directory is in the Python path
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if path not in sys.path:
+    sys.path.append(path)
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gyan_uday.settings')
